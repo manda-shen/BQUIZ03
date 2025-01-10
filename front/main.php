e0d3129ad58d952879e564d395d1a8fa03eb17e5
Manda <mandashen@gmail.com>
2025-01-10 16:37:42 +0800

    建立預告片下方按鈕區佈局及css、完成預告版按鈕區顯示及CSS設定、完成預告片自動輪播轉場

--- front/main.php
+++ front/main.php
@@ -1,14 +1,147 @@
+<style>
+  .poster-block{
+    margin:0;
+    padding:0;
+    font-size:12px;
+    box-sizing:border-box;
+  }
+  .lists{
+    width:210px;
+    height:280px;
+    margin:auto;
+    position:relative;
+  }
+  .controls{
+    width: 100%;
+    height: 100px;
+    margin:10px auto;
+    display:flex;
+    align-items:center;
+    justify-content:space-around;
+  }
+
+  .poster{
+    position:absolute;
+    display:none;
+    text-align: center;
+  }
+
+  .poster img{
+    display:block;
+    width: 210px;
+    height: 250px;
+  }
+  
+  .poster span{
+    font-size:16px;
+  }
+  .left ,.right{
+    width:0;
+    border-top:15px solid transparent;
+    border-bottom:15px solid transparent;
+  }
+  .left{
+    border-right:25px solid #eee;
+    border-left:0;
+  }
+  .right{
+    border-left:25px solid #eee;
+    border-right:0;
+  }
+  .icons{
+    width: 340px;
+    display:flex;
+    overflow:hidden;
+    position:relative;
+  }
+  .icon{
+    width: 85px;
+    height: 110px;
+    flex-shrink:0;
+    text-align:center;
+    position:relative;
+  }
+  .icon img{
+    width: 80px;
+    height: 95px;
+  }
+  .icon div{
+    font-size:12px;
+  }
+</style>
+
 <div class="half" style="vertical-align:top;">
-      <h1>預告片介紹</h1>
-      <div class="rb tab" style="width:95%;">
-        <div id="abgne-block-20111227">
-          <ul class="lists">
-          </ul>
-          <ul class="controls">
-          </ul>
+  <h1>預告片介紹</h1>
+  <div class="rb tab" style="width:95%;">
+    <div class="poster-block">
+      <div class="lists">
+          <?php
+          $posters=$Poster->all(['sh'=>1]," order by rank");
+          foreach($posters as $idx => $poster):
+          ?>
+          <div class="poster" data-ani="<?=$poster['ani'];?>">
+            <img src="./upload/<?=$poster['img']; ?>">
+            <span><?=$poster['name']; ?></span>
+          </div>
+          <?php
+          endforeach;
+          ?>
+      </div>
+      <div class="controls">
+        <div class="left"></div>
+        <div class="icons">
+          <?php
+          foreach($posters as $idx => $poster):
+          ?>
+          <div class="icon">
+            <img src="./icon/<?=$poster['img'] ?>" alt="">
+            <div><?=$poster['name']; ?></div>
+          </div>
+          <?php
+          endforeach;
+          ?>
         </div>
+        <div class="right"></div>
       </div>
     </div>
+  </div>
+</div>
+
+<script>
+  $(".poster").eq(0).show();
+
+  let slider=setInterval(() => {
+    sliders();
+}, 2500);
+
+  function sliders(){
+    let now=$(".poster:visible").index();
+    let next=($(".poster").length==now+1)?0:now+1;
+    let ani=$(".poster").eq(next).data('ani');
+    //console.log(now,next)
+
+    switch(ani){
+      case 1:
+        // 淡入淡出
+        $(".poster").eq(now).fadeOut(1000,function(){
+          $(".poster").eq(next).fadeIn(1000);
+        });
+      break;
+      case 2:
+        // 縮放
+        $(".poster").eq(now).hide(1000,function(){
+          $(".poster").eq(next).show(1000);
+        });
+      break;
+      case 3:
+        // 滑入滑出
+        $(".poster").eq(now).slideUp(1000,function(){
+          $(".poster").eq(next).slideDown(1000);
+        });
+      break;
+    }
+  }
+</script>
     <div class="half">
       <h1>院線片清單</h1>
       <?php
@@ -28,16 +161,17 @@
       ?>
       <style>
         .movie-item{
-          width:49%;
-          border:1px solid #ccc;
-          border-radius:5px;
-          margin:0.25%;
-          display: flex;
-          flex-wrap: wrap;
-          padding: 3px;
-          box-sizing: border-box;
-          font-size:14px;
-          align-content:center;
+        width: 49%;
+        height: 190px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        margin: 0.25%;
+        display: flex;
+        flex-wrap: wrap;
+        padding: 3px;
+        box-sizing: border-box;
+        font-size:12px;
+        align-content:center;
         }
       </style>
       <div class="rb tab" style="width:95%;">
